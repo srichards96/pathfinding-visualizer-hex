ad4ff0c5fa65ef0242e5af15717f86001b7bf4f6
Scott Richards <scottrichards789@gmail.com>
2025-03-29 18:43:53 +0000

    Added PriorityQueue. Updated Dijkstra's algorithm to use it
    Updated Dijkstra's to include a hack using tiny offsets so that the priority queue maintains insert order of neighbors with equal distance...

--- src/algorithms/hex-grid-dijkstras-algorithm.ts
+++ src/algorithms/hex-grid-dijkstras-algorithm.ts
@@ -1,3 +1,4 @@
+import { PriorityQueue } from "../data-structures/priority-queue";
 import { HexGridCellType } from "../types/hex-grid-cell-type";
 import { HexGridPathfindingAlgorithm } from "../types/hex-grid-pathfinding-algorithm";
 import { map2d } from "../util/array/map-2d";
@@ -5,11 +6,11 @@ import { getTraversal } from "../util/graph/get-traversal";
 import { getHexNeighbors } from "../util/hex/get-hex-neighbors";
 
 type DijkstrasNode = {
+  id: number;
   x: number;
   y: number;
   wall: boolean;
   weight: number;
-  distance: number;
   visited: boolean;
   parent: DijkstrasNode | null;
 };
@@ -20,14 +21,17 @@ export const hexGridDijkstrasAlgorithm: HexGridPathfindingAlgorithm = ({
   target,
   wideRows,
 }) => {
+  let id = 0;
+  let neighborCounter = 0;
+
   const dijkstrasGrid = map2d<HexGridCellType, DijkstrasNode>(
     grid,
     ({ x, y, wall, weight }) => ({
+      id: id++,
       x,
       y,
       wall,
       weight,
-      distance: Infinity,
       visited: false,
       parent: null,
     })
@@ -36,21 +40,20 @@ export const hexGridDijkstrasAlgorithm: HexGridPathfindingAlgorithm = ({
   const traversedNodes: DijkstrasNode[] = [];
   let endNode: DijkstrasNode | null = null;
 
-  // Mark start as having distance of 0 and start priority queue with it
-  dijkstrasGrid[start.y][start.x].distance = 0;
+  // Create priority queue and add start position to it
+  const priorityQueue = new PriorityQueue({
+    getKeyFn: (item: DijkstrasNode) => item.id,
+  });
   dijkstrasGrid[start.y][start.x].visited = true;
+  priorityQueue.add(dijkstrasGrid[start.y][start.x], 0);
 
-  const queue = [dijkstrasGrid[start.y][start.x]];
+  while (priorityQueue.size > 0) {
+    const { data: curr, priority: currDistance } = priorityQueue.pull()!;
 
-  while (queue.length > 0) {
-    queue.sort((a, z) => a.distance - z.distance);
-
-    const curr = queue.shift()!;
-
-    // If a node with a distance of infinity has been reached, the target is unreachable
-    if (curr.distance === Infinity) {
-      break;
-    }
+    // // If a node with a distance of infinity has been reached, the target is unreachable
+    // if (curr.distance === Infinity) {
+    //   break;
+    // }
 
     traversedNodes.push(curr);
 
@@ -72,12 +75,27 @@ export const hexGridDijkstrasAlgorithm: HexGridPathfindingAlgorithm = ({
       // Not a wall
       .filter((x) => x.wall === false);
 
-    for (const neighbor of neighbors) {
+    for (let i = 0; i < neighbors.length; i++) {
+      const neighbor = neighbors[i];
+
       if (!neighbor.visited) {
+        // This offset isn't necessary.
+        // It's a hack that makes it so that the nodes with equal distance are visited in the
+        //   same order that they were added in (top, top-right, bottom-right, bottom, bottom-left, top-left)
+        // Because the PriorityQueue does not maintain insert-order of items with equal priority,
+        //   neighbors added with the same priority won't be retrieved in the same order.
+        // E.g. top, top-right, and bottom-right might all have a priority of 1, but they won't
+        //   necessarily be retrived in the order top, top-right, bottom-right
+        // So each priority is globally offset by some tiny amount, to maintain insert-order...
+        // So then, top will have priority 1.000000, top-right 1.0000001, bottom-right 1.0000002,
+        //   so they'll be retrived in order top, top-right, bottom-right...
+        // This doesn't interfere with the search because weights are always integers
+        const offset = neighborCounter++ * 0.0000001;
+
+        const priority = Math.floor(currDistance) + neighbor.weight;
         neighbor.visited = true;
-        neighbor.distance = curr.distance + neighbor.weight;
         neighbor.parent = curr;
-        queue.push(neighbor);
+        priorityQueue.add(neighbor, priority + offset);
       }
     }
   }
---
+++ src/data-structures/priority-queue.ts
@@ -0,0 +1,205 @@
+type GetKeyFn<TItem, TKey extends number | string> = (item: TItem) => TKey;
+
+type PriorityQueueItem<TItem> = {
+  priority: number;
+  data: TItem;
+};
+
+type ConstructorArgs<TItem, TKey extends number | string> = {
+  /**
+   * Gets unique key value for each item
+   */
+  getKeyFn: GetKeyFn<TItem, TKey>;
+};
+
+export class PriorityQueue<TItem, TKey extends number | string> {
+  private getKeyFn: GetKeyFn<TItem, TKey>;
+  private array: TItem[] = [];
+
+  // Maps from item key to index in items array
+  private keyToIndexMap: Map<TKey, number> = new Map();
+  // Maps from item index to priority value
+  private indexToPriorityMap: Map<number, number> = new Map();
+
+  constructor({ getKeyFn }: ConstructorArgs<TItem, TKey>) {
+    this.getKeyFn = getKeyFn;
+  }
+
+  private indexToKey(index: number) {
+    return this.getKeyFn(this.array[index]);
+  }
+
+  private indexToPriority(index: number) {
+    const priority = this.indexToPriorityMap.get(index);
+    if (priority === undefined) {
+      throw `No priority exists for index '${index}'`;
+    }
+    return priority;
+  }
+
+  private keyToIndex(key: TKey) {
+    const index = this.keyToIndexMap.get(key);
+    if (index === undefined) {
+      throw `No index eixsts for key '${key}'`;
+    }
+    return index;
+  }
+
+  private getParentIndex(index: number) {
+    return Math.floor((index - 1) / 2);
+  }
+  private getLeftChildIndex(index: number) {
+    return index * 2 + 1;
+  }
+  private getRightChildIndex(index: number) {
+    return index * 2 + 2;
+  }
+
+  private hasParent(index: number) {
+    return this.getParentIndex(index) >= 0;
+  }
+  private hasLeftChild(index: number) {
+    return this.getLeftChildIndex(index) < this.array.length;
+  }
+  private hasRightChild(index: number) {
+    return this.getRightChildIndex(index) < this.array.length;
+  }
+
+  private getParentPriority(index: number) {
+    return this.indexToPriority(this.getParentIndex(index));
+  }
+  private getLeftChildPriority(index: number) {
+    return this.indexToPriority(this.getLeftChildIndex(index));
+  }
+  private getRightChildPriority(index: number) {
+    return this.indexToPriority(this.getRightChildIndex(index));
+  }
+
+  private heapifyDown(index: number = 0) {
+    if (this.array.length === 0) {
+      return;
+    }
+
+    while (this.hasLeftChild(index)) {
+      const childIndex =
+        this.hasRightChild(index) &&
+        this.getRightChildPriority(index) < this.getLeftChildPriority(index)
+          ? this.getRightChildIndex(index)
+          : this.getLeftChildIndex(index);
+
+      if (this.indexToPriority(index) <= this.indexToPriority(childIndex)) {
+        break;
+      }
+
+      this.swap(index, childIndex);
+      index = childIndex;
+    }
+  }
+
+  private heapifyUp(index?: number) {
+    if (this.array.length === 0) {
+      return;
+    }
+
+    index ??= this.array.length - 1;
+
+    while (
+      this.hasParent(index) &&
+      this.getParentPriority(index) > this.indexToPriority(index)
+    ) {
+      this.swap(this.getParentIndex(index), index);
+      index = this.getParentIndex(index);
+    }
+  }
+
+  private pop(): PriorityQueueItem<TItem> | undefined {
+    if (this.array.length === 0) {
+      return undefined;
+    }
+
+    const index = this.array.length - 1;
+    const key = this.indexToKey(index);
+
+    const data = this.array.pop()!;
+    const priority = this.indexToPriority(index);
+    this.keyToIndexMap.delete(key);
+    this.indexToPriorityMap.delete(index);
+
+    return { priority, data };
+  }
+
+  private swap(index1: number, index2: number) {
+    const key1 = this.indexToKey(index1);
+    const key2 = this.indexToKey(index2);
+
+    // Swap items
+    const itemTemp = this.array[index1];
+    this.array[index1] = this.array[index2];
+    this.array[index2] = itemTemp;
+
+    // Swap key-to-index mappings
+    const indexTemp = this.keyToIndex(key1);
+    this.keyToIndexMap.set(key1, this.keyToIndexMap.get(key2)!);
+    this.keyToIndexMap.set(key2, indexTemp);
+
+    // Swap index-to-priority mappings
+    const priorityTemp = this.indexToPriority(index1);
+    this.indexToPriorityMap.set(index1, this.indexToPriorityMap.get(index2)!);
+    this.indexToPriorityMap.set(index2, priorityTemp);
+  }
+
+  public add(item: TItem, priority: number) {
+    const index = this.array.push(item) - 1;
+    const key = this.getKeyFn(item);
+
+    this.keyToIndexMap.set(key, index);
+    this.indexToPriorityMap.set(index, priority);
+
+    this.heapifyUp();
+  }
+
+  public peek(): PriorityQueueItem<TItem> | undefined {
+    if (this.array.length === 0) {
+      return undefined;
+    }
+
+    const priority = this.indexToPriority(0);
+    const data = this.array[0];
+
+    return { priority, data };
+  }
+
+  public pull(): PriorityQueueItem<TItem> | undefined {
+    if (this.array.length === 0) {
+      return undefined;
+    }
+
+    this.swap(0, this.array.length - 1);
+    const result = this.pop();
+    this.heapifyDown();
+
+    return result;
+  }
+
+  public setPriority(key: TKey, newPriority: number) {
+    const index = this.keyToIndex(key);
+    const oldPriority = this.indexToPriority(index);
+
+    if (newPriority === oldPriority) {
+      return;
+    }
+
+    this.indexToPriorityMap.set(index, newPriority);
+
+    // Lower value = "higher" priority
+    if (newPriority < oldPriority) {
+      this.heapifyUp(index);
+    } else {
+      this.heapifyDown(index);
+    }
+  }
+
+  get size() {
+    return this.array.length;
+  }
+}
